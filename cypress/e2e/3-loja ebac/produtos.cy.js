ecfb4d3052f501800bb1d12d9136d76e2e48c729
jeffSertao <sertjeff@yahoo.com.br>
2024-05-08 00:34:21 -0300

    Massa de dados em lista

--- cypress/e2e/3-loja ebac/produtos.cy.js
+++ cypress/e2e/3-loja ebac/produtos.cy.js
@@ -13,15 +13,36 @@ describe('Funcionalidade: Produtos', () => {
         cy.get('#tab-title-description > a').should('exist')
         
     });
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         let produto='Aero Daily Fitness Tee'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
     });
     it('Deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
+        cy.get('.product_title').should('contain', 'Aero Daily Fitness Tee' )
+
         
     });
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Blue', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
+
+        
+    });
+    it.only('Deve adicionar produto ao carrinho - usando massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+        })
+        
         
     });
 });
